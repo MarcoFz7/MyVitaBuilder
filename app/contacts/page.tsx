344ba91bc4d33b42e731f14218df8bc975d68eff
1200942MarcoFerraz <1200942@isep.ipp.pt>
2024-05-02 23:02:17 +0100

    #comment creating other navigation pages to prevent 404 error and test navigation

---
+++ app/contacts/page.tsx
@@ -0,0 +1,12 @@
+import './page.css'
+
+const Page = () => {
+  
+  return (
+    <div className="contacts-page">
+      Contacts page
+    </div>
+  );
+};
+  
+export default Page;
---
+++ app/goals/page.tsx
@@ -0,0 +1,12 @@
+import './page.css'
+
+const Page = () => {
+  
+  return (
+    <div className="goals-page">
+      Goals page
+    </div>
+  );
+};
+  
+export default Page;
