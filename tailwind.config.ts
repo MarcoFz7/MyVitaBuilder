c9d08f56357a52edd4c9ded5e0047a19124cdb9a
Marco Ferraz <marcoantoniopferraz@gmail.com>
2024-05-30 22:17:10 +0100

    #comment Adding some customized animations using tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        rotate360: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+        rotate360minus: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(-360deg)' },
+        },
+      },
+      animation: {
+        'rotate-360': 'rotate360 0.5s linear',
+        'rotate-360-minus': 'rotate360minus 0.5s linear',
+      },
     },
   },
   plugins: [],
