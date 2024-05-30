9577f207aebaca05fc1e0889fd1dfc5bbb1d7f6f
Marco Ferraz <marcoantoniopferraz@gmail.com>
2024-05-30 22:16:47 +0100

    #comment Adding more content to the nutrition page and changing the page structure (some classes were changed from grid to flex)

--- app/nutrition/page.tsx
+++ app/nutrition/page.tsx
@@ -6,7 +6,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
 import { FaDrumstickBite, FaUser, FaReceipt } from "react-icons/fa";
-import { IoScanSharp  } from "react-icons/io5";
+import { IoScanSharp, IoCheckmark  } from "react-icons/io5";
 import { RiRobot2Fill } from "react-icons/ri";
 import { Inter } from 'next/font/google';
 
@@ -68,6 +68,16 @@ const Page = () => {
   const [selectedCalorieIntakeOption, setSelectedCalorieIntakeOption] = useState<Option[]>([]);
   const [selectedAllergiesAndIntoleranceOptions, setSelectedAllergiesAndIntoleranceOptions] = useState<Option[]>([]);
   const [ingredientsTextAreaValue, setIngredientsTextAreaValue] = useState<string>("");
+  
+  /*
+    Consts related to the answer text area
+  */
+  const [isRotated, setIsRotated] = useState<boolean>(false);
+  const [answerReceived, setAnswerReceived] = useState<boolean>(false);
+  const [displayedAnswer, setDisplayedAnswer] = useState<string>('');
+  const [lastAnswer, setLastAnswer] = useState<string>('This is a answer sample test!');
+  const [fullAnswer, setFullAnswer] = useState<string[]>([]); 
+  const [numberOfAnswers, setNumberOfAnswers] = useState<number>(0); 
 
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
@@ -124,18 +134,65 @@ const Page = () => {
   useEffect(() => {
     setTextAreaHeight(); 
   }, [ingredientsTextAreaValue]);
+
+  const handleRobotIconAnimation = () => {
+    setIsRotated(!isRotated);
+  };
+
+  const handleTextTypingAnimation = () => {
+    if (!lastAnswer) 
+      return;
+
+    setDisplayedAnswer('');
+
+    let currentIndex = -1;
+
+    const intervalId = setInterval(() => {
+      setDisplayedAnswer((prev) => {
+        if (currentIndex < lastAnswer.length) {
+          return prev + lastAnswer[currentIndex];    
+        } else {
+          clearInterval(intervalId);
+          return prev;
+        }
+      });
+      currentIndex++;
+    }, 10);
+
+    return () => { clearInterval(intervalId);}; 
+  };
+
+  const handleRequestConfirmation = () => {
+    console.log("CONFIRM BTN");
+
+    // All this stuff is to be done when an answer is actually received from the back end, not when the request is sent!
+    console.log("WL:" + isWeightLossSelected + " ;WG: " + isWeightGainSelected); 
+    setAnswerReceived(true); 
+    handleTextTypingAnimation();
+    setFullAnswer(prevState => [...prevState, "This is a answer sample!"]);
+    setNumberOfAnswers(numberOfAnswers + 1);
+
+    setTimeout(() => {
+      const lastAnswer = document.getElementById('lastAnswer');
+
+      if (lastAnswer) {
+        lastAnswer.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 50);
+  }
   
   return (
     <div className="nutrition-page">
-      <div className="page-manual-calculator"></div>
-      <div className="page-ai-calculator">
-        <span className={`${inter.className} ai-calculator-header`} onClick={() => console.log("WL:" + isWeightLossSelected + " ;WG: " + isWeightGainSelected)}>
-          Get <strong>better results</strong> through your meals and preparation using <strong>AI</strong>!
+      <div className="page-manual-calculator">
+      </div>
+      <div className="page-ai-calculator" onMouseEnter={handleRobotIconAnimation} onMouseLeave={handleRobotIconAnimation}>
+        <span className={`${inter.className} ai-calculator-header`}>
+          Get <b>better results</b> through your meals and preparation using <strong>AI</strong>!
         </span>
         <div className={`${inter.className} ai-calculator-info`}>
           <div>
-            <label className="block mb-1.5 text-sm text-white-900 pt-px">Please configure this section accordingly.</label>
-            <span className="icon-and-title mt-1 ml-1"><FaUser/><strong>Personal information</strong></span>
+            <label className="block mb-1.5 text-sm text-white-900 pt-px ml-1.5">Please configure this section accordingly.</label>
+            <span className="icon-and-title mt-1 ml-1"><FaUser className='icon-color'/><strong>Personal information</strong></span>
             <div className='weight-objective ml-1'>
               <div className="flex items-center">
                 <input id="weight-loss-radio" title='weight-loss-radio' type="radio" value="" name="weight-radio" className="w-4 h-4 focus:ring-2 pb-2" checked={isWeightLossSelected} onChange={handleWeightLossRadioClick} onClick={handleWeightLossRadioClick}></input>
@@ -159,7 +216,7 @@ const Page = () => {
               onChange={handleSelectAllergiesAndIntoleranceOptions}
             />
             <label className="block mt-2 mb-1.5 text-sm text-white-900 text-sm pt-px"></label>
-            <span className="icon-and-title mt-1 ml-1"><FaDrumstickBite/><strong>Meal information</strong></span>
+            <span className="icon-and-title mt-1 ml-1"><FaDrumstickBite className='icon-color'/><strong>Meal information</strong></span>
             <Select className='select'
               closeMenuOnSelect={false}
               blurInputOnSelect={false}
@@ -192,19 +249,43 @@ const Page = () => {
           <div className="ml-1">
             <label className="block mt-1.5 mb-1.5 text-sm text-white-900 pt-px"></label>
             <div className='flex items-center justify-between w-full'>
-              <span className="icon-and-title mt-1 ml-0"><FaReceipt className=''/><strong className='truncate'>Enter ingredients or...</strong></span>
-              <button type='button' className='photo-scan-btn rounded p-1 pl-1.5 ml-1.5' title='Photograph Ingredients' onClick={setIngredientsTextArea}>
+              <span className="icon-and-title mt-1 ml-0"><FaReceipt className='icon-color mb-px'/><strong className='truncate'>Enter ingredients or...</strong></span>
+              <button type='button' className='photo-scan-btn rounded p-1 pl-1.5 pr-1.5 ml-1.5' title='Photograph Ingredients' onClick={setIngredientsTextArea}>
                 <span className="icon-and-title right mt-1"><strong>Scan</strong><IoScanSharp /></span>
               </button>
             </div>
             <textarea ref={textareaRef} placeholder='ex. 1 Banana, 1 egg ...' className='ingredients-input rounded mt-1 p-1 pl-1.5 pr-1.5 w-full h-8 min-h-8' value={ingredientsTextAreaValue} onChange={handleTextAreaValueChange}></textarea>
           </div>
+          <div>
+            <button type='button' className='confirm-btn rounded p-1 pl-1.5 pr-1.5 ml-1 mt-0.5 w-fit' title='Confirm!' onClick={handleRequestConfirmation}>
+              <span className="icon-and-title right mt-1"><strong>Confirm</strong><IoCheckmark/></span>
+            </button>
+          </div>
           <div className="ml-1 h-min flex-1 mb-4 min-h-24">
             <label className="block mt-1.5 mb-1.5 text-sm text-white-900 pt-px bg-white/50"></label>
             <div className='h-full w-full rounded-sm bg-white/50 flex flex-col justify-end'>
-              <div className='flex flex-row'>
-                <RiRobot2Fill className='text-black text-xl/5 justify-start mt-2 ml-1.5'/><span className='text-black mt-1 p-1 pl-1.5 pr-1.5'>Test span for answer</span>
-              </div>           
+              <div className={`flex flex-col p-1 ${answerReceived ? 'overflow-y-auto' : 'overflow-y-hidden'}`}>
+                {!answerReceived ? (
+                  <div className='flex flex-col items-center transition-all duration-1000 ease'>
+                    <RiRobot2Fill className={`${isRotated ? 'animate-rotate-360' : 'animate-rotate-360-minus'} w-16 h-16 text-black justify-start mt-1.5 ml-1.5 mr-1 transition-all duration-1000 ease`} onClick={handleRobotIconAnimation}/>
+                  </div> 
+                ) : (
+                  <>
+                    {
+                      fullAnswer.map((answer, index) => (
+                        <div key={`${answer}-${index}`} className='flex flex-row pt-0.5 transition-all duration-1000 ease'>
+                          <RiRobot2Fill className='w-6 h-6 min-w-6 min-h-6 text-black justify-start mt-1.5 ml-1.5 mr-1 transition-all duration-1000 ease'/>
+                          {numberOfAnswers != (index + 1) ? (
+                            <span className='text-black mt-1 p-1 pl-1.5 pr-1.5 transition-all duration-1000 ease'>{answer}</span>
+                          ) : (
+                            <span id='lastAnswer' className='text-black mt-1 p-1 pl-1.5 pr-1.5 transition-all duration-1000 ease'>{displayedAnswer}</span>
+                          )}
+                        </div>   
+                      ))
+                    }                    
+                  </> 
+                )}                       
+              </div>         
             </div>
           </div>
         </div>
