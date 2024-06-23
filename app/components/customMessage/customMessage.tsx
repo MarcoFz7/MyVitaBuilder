bc8bae7a1c7098a6af9a5beabf2c0741f14b9cfe
Marco Ferraz <marcoantoniopferraz@gmail.com>
2024-06-23 21:25:43 +0100

    #comment Addition of a new custom component, a custom message to interact with the user for a better experience - as of now, there is the error, warning and info message.

---
+++ app/components/customMessage/customMessage.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import { IoWarning } from "react-icons/io5";
+
+/**
+ * Interface for the custom message with needed props from the parent
+ */
+interface customMessageProps {
+    type: number;
+    message: string;
+    iconSize: string;
+    isOpacityOne: boolean;
+    onTransitionEnd: () => void;
+}
+
+/**
+ * 
+ * @param type - type of the custom message: 1-Info, 2-Warning, 3-Error
+ * @param message - content to use in the custom message
+ * @param size - size of the icon
+ * @param changeOpacity - controls component visibility (mainly to apply smoth transitions)
+ * @param onTransitionEnd - callback function to invoke when the transition ends and hide message
+ * 
+ * @returns custom 
+ */
+const CustomMessage = ({ type, message, iconSize, isOpacityOne, onTransitionEnd }: customMessageProps) => { 
+   
+  const [opacity, setOpacity] = useState<number>(0);
+
+  useEffect(() => {
+    let hideTimeout: NodeJS.Timeout;
+  
+    if (isOpacityOne) {
+      setOpacity(1);
+
+      hideTimeout = setTimeout(() => {
+        setOpacity(0);
+      }, 20000500);
+  
+      setTimeout(() => {
+        onTransitionEnd();
+      }, 20000750);
+    }
+  
+    return () => {
+      clearTimeout(hideTimeout);
+    };
+  }, [isOpacityOne]);
+
+  return (
+    <>
+        { type == 1 &&
+            <div className={`flex w-[100%] sm:w-[92%] h-[70%] justify-center self-center items-center text-[12px] text-green-900 bg-green-100 rounded opacity-${opacity} transition-all duration-250 ease`} role="alert">
+                <span className="flex items-center whitespace-nowrap overflow-hidden pr-1"><IoWarning className={`inline-block mr-[0.150rem] sm:mr-[0.375rem] ${iconSize}`}/><strong className="overflow-hidden text-ellipsis">Information! {message}</strong></span>
+            </div>
+        }
+        { type == 2 &&
+            <div className={`flex w-[100%] sm:w-[92%] h-[70%] justify-center self-center items-center text-[12px] text-yellow-900 bg-yellow-100 rounded opacity-${opacity} transition-all duration-250 ease`} role="alert">
+                <span className="flex items-center whitespace-nowrap overflow-hidden pr-1"><IoWarning className={`inline-block mr-[0.150rem] sm:mr-[0.375rem] ${iconSize}`}/><strong className="overflow-hidden text-ellipsis">Warning! {message}</strong></span>
+            </div>
+        }
+        { type == 3 &&
+            <div className={`flex w-[100%] sm:w-[92%] h-[70%] justify-center self-center items-center text-[12px] text-red-900 bg-red-100 rounded opacity-${opacity} transition-all duration-250 ease`} role="alert">
+                <span className="flex items-center whitespace-nowrap overflow-hidden pr-1"><IoWarning className={`inline-block mr-[0.150rem] sm:mr-[0.375rem] ${iconSize}`}/><strong className="overflow-hidden text-ellipsis">Error! {message}</strong></span>
+            </div>
+        }
+    </>
+  );
+};
+  
+export default CustomMessage;         
