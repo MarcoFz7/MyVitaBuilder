6021c8ba22c212fb1cde6af3ba15566c7454bf0f
Marco Ferraz <marcoantoniopferraz@gmail.com>
2024-06-23 21:24:56 +0100

    #comment Additions to the custom text area to allow its reset. This is to be applied to the custom input also.

--- app/components/customTextArea/customTextArea.tsx
+++ app/components/customTextArea/customTextArea.tsx
@@ -13,6 +13,10 @@ interface CustomTextAreaProps {
     // Callback function to handle comunication between parent and child. 
     // In this case this component triggers it and sends the value to the parent, the parent does nothing besides receiving the value
     onTextareaValueRequest: (textareaValue: string) => void;
+
+    resetRequest: boolean;
+    // Callback function to inform parent value was reseted
+    onResetCompleted: () => void;
 }
 
 /**
@@ -22,20 +26,12 @@ interface CustomTextAreaProps {
  * @param maxCharacters - option to allow to specify the max allowed characters for the textarea 
  * @param isDisabled - option to allow the input to be editable, or not
  * @param onTextareaValueRequest - callback function to pass the value to the parent 
+ * @param resetRequest - option that indicates a request from the parent to reset the textarea value
+ * @param osResetCompleted - callback function to reset the variable "resetRequest" allowing to multiple resets
  * 
  * @returns custom textarea
  */
-const CustomTextArea = ({ placeholder, isSizeRestricted, maxCharacters, isDisabled, isResizable, onTextareaValueRequest }: CustomTextAreaProps) => {
-  const [isParentToBeUpdated, setisParentToBeUpdated] = useState<boolean>(false);
-
-  // Handle sending response to parent based on 'request' change - sends textarea current value
-  useEffect(() => {
-    if (isParentToBeUpdated) {
-        onTextareaValueRequest(descriptionTextareaInputValue);
-
-        setisParentToBeUpdated(false);
-    }
-  }, [isParentToBeUpdated]);
+const CustomTextArea = ({ placeholder, isSizeRestricted, maxCharacters, isDisabled, isResizable, onTextareaValueRequest, resetRequest, onResetCompleted }: CustomTextAreaProps) => {
 
   const [descriptionTextareaInputValue, setDescriptionTextareaInputValue] = useState('');
   const [descriptionTextareaCharCount, setDescriptionTextareaCharCount] = useState(0);
@@ -58,8 +54,10 @@ const CustomTextArea = ({ placeholder, isSizeRestricted, maxCharacters, isDisabl
     }
 
     if (newValue.length <= maxDescriptionTextAreaCharacters) {
-        setDescriptionTextareaInputValue(newValue);
-        setDescriptionTextareaCharCount(newValue.length);
+        setDescriptionTextareaInputValue(newValue.trimStart());
+        setDescriptionTextareaCharCount(newValue.trimStart().length);
+
+        updateValue(newValue.trimStart());
     } else {
         setDescriptionTextareaBlocked(true);
 
@@ -69,13 +67,25 @@ const CustomTextArea = ({ placeholder, isSizeRestricted, maxCharacters, isDisabl
     }
   };
 
-  const handleUserChanges = () => {
-    setisParentToBeUpdated(true);
+  const updateValue = (newValue: string) => {
+    onTextareaValueRequest(newValue);
   }
+
+  /**
+   * UseEffect to reset textarea value
+   */
+  useEffect(() => {
+    if (resetRequest) {
+      setDescriptionTextareaInputValue('');
+      setDescriptionTextareaCharCount(0);
+      onResetCompleted();
+    }
+  }, [resetRequest]);
+
 
   return (
     <>
-        <textarea placeholder={placeholder} disabled={isDisabled} onBlur={handleUserChanges} className={`text-black pt-[0.38rem] placeholder:text-black placeholder:opacity-50 rounded p-1 pl-1.5 pr-1.5 w-full h-full ${!isResizable ? 'resize-none' : ''}`} value={descriptionTextareaInputValue} onChange={handleTextAreaInputChange}>
+        <textarea placeholder={placeholder} disabled={isDisabled} className={`text-black pt-[0.38rem] placeholder:text-black placeholder:opacity-50 rounded p-1 pl-1.5 pr-1.5 w-full h-full ${!isResizable ? 'resize-none' : ''}`} value={descriptionTextareaInputValue} onChange={handleTextAreaInputChange}>
         </textarea>
         {isSizeRestricted && descriptionTextareaCharCount != 0 && (
             <div className='flex items-center bg-white rounded pl-1 w-min h-full ml-0.5'>
