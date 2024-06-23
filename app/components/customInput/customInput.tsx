80c82a0fbff4f3d59cbf9089aeaef8f852f39416
Marco Ferraz <marcoantoniopferraz@gmail.com>
2024-06-23 21:23:30 +0100

    #comments Small changes on the custom input, mainly to change the space ocuppies by custom input in sequence

--- app/components/customInput/customInput.tsx
+++ app/components/customInput/customInput.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 /**
  * Interface for the custom component with needed props from the parent
  */
@@ -8,6 +10,7 @@ interface CustomInputProps {
     topic: string;
     hasUnit: boolean;
     unit: string;
+    inSequence: boolean;
 }
 
 /**
@@ -18,22 +21,24 @@ interface CustomInputProps {
  * @param topic - topic of the field 
  * @param hasUnit - option to allow to specify the unit of the topic, in case it has a unit, ex. Calories - kcal
  * @param unit - unit of the input
+ * @param inSequence - option to change input when its used in sequence with another one
  * 
  * @returns custom input
  */
-const CustomInput = ({ title, placeholder, isDisabled, topic, hasUnit, unit }: CustomInputProps) => {
+const CustomInput = ({ title, placeholder, isDisabled, topic, hasUnit, unit, inSequence }: CustomInputProps) => { 
+  const [inputValue, setInputValue] = useState<string>('');
   
   return (
     <>
-        <div className='h-full p-1.5 sm:p-2 min-h-[32px]'>
-            <div className='flex flex-row gap-1 w-full h-full bg-white rounded p-1 pt-0.5 pb-0.5 pr-0.5'>
-            <input title={title} placeholder={placeholder} disabled={isDisabled} className='flex text-black h-full w-1/3 items-center rounded'/>
-            <div className='target flex w-auto h-full bg-c-dark-green rounded p-0.5 pl-1 pr-1 gap-1.5 ml-auto'>
-                <span className='flex items-center h-full font-bold text-xs'>{topic}</span>
-                {hasUnit && (
-                    <span className='flex items-center h-full bg-c-lemon-green rounded text-c-dark-green pl-1 pr-1 font-bold text-xs'>{unit}</span>
-                )}
-            </div>
+        <div className={`h-full min-h-[32px] p-1.5 sm:p-2 ${inSequence && '!pb-0'}`}>
+            <div className='flex flex-row justify-between gap-1 w-full h-full rounded p-1 pt-0.5 pb-0.5 pr-0.5 bg-c-light-smoke shadow-inner'>
+                <input title={title} placeholder={placeholder} disabled={isDisabled} value={inputValue} className='flex flex-grow text-black h-full pl-0.5 w-1/3 items-center rounded pt-0.5'/>
+                <div className='flex w-auto h-full bg-c-dark-green rounded p-0.5 pl-1 pr-1 gap-1'>
+                    <span className='flex items-center h-full font-bold text-xs whitespace-nowrap overflow-hidden text-ellipsis'>{topic}</span>
+                    {hasUnit && (
+                        <span className='flex items-center h-full bg-c-light-smoke rounded text-c-dark-green pl-1 pr-1 font-bold text-xs'>{unit}</span>
+                    )}
+                </div>
             </div>
         </div>
     </>
