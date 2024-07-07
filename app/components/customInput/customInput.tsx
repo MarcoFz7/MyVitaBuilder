fe4c73b4cc00b532934ee4de95bf3434dd5f39c4
Marco Ferraz <marcoantoniopferraz@gmail.com>
2024-07-07 20:31:17 +0100

    #comment Small changes related to the colors and shadow of the custom input - using the custom shadow

--- app/components/customInput/customInput.tsx
+++ app/components/customInput/customInput.tsx
@@ -30,12 +30,12 @@ const CustomInput = ({ value, title, placeholder, isDisabled, topic, hasUnit, un
   return (
     <>
         <div className={`h-full min-h-[32px] p-1.5 sm:p-2 ${inSequence && '!pb-0'}`}>
-            <div className='flex flex-row justify-between gap-1 w-full h-full rounded p-1 pt-0.5 pb-0.5 pr-0.5 bg-c-light-smoke shadow-inner'>
-                <input title={title} placeholder={placeholder} disabled={isDisabled} value={value} className='flex flex-grow text-black h-full pl-0.5 w-1/3 items-center rounded pt-0.5'/>
+            <div className='flex flex-row justify-between gap-1 w-full h-full rounded p-1 pt-0.5 pb-0.5 pr-0.5 border border-grey-100 shadow-customShadow'>
+                <input title={title} placeholder={placeholder} disabled={isDisabled} value={value} className='flex flex-grow text-black h-full pl-0.5 w-1/3 items-center rounded pt-0.5 bg-transparent'/>
                 <div className='flex w-auto h-full bg-c-dark-green rounded p-0.5 pl-1 pr-1 gap-1'>
                     <span className='flex items-center h-full font-bold text-xs whitespace-nowrap overflow-hidden text-ellipsis'>{topic}</span>
                     {hasUnit && (
-                        <span className='flex items-center h-full bg-c-light-smoke rounded text-c-dark-green pl-1 pr-1 font-bold text-xs'>{unit}</span>
+                        <span className='flex items-center h-full bg-c-lemon-green rounded text-c-dark-green pl-1 pr-1 font-bold text-xs'>{unit}</span>
                     )}
                 </div>
             </div>
